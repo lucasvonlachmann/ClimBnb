2422412da7dbb14f1acacddb2d7e400a0cac129c
Christophe Nonato <chr.nonato@protonmail.com>
2019-03-12 16:32:20 -0300

    calendar updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,10 @@
 import "bootstrap";
- // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+// CSS
+// import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initFlatpickr } from '../plugins/flatpickr'
 
 initMapbox();
+initFlatpickr();
